8546c5289397deb26d5bea778b810778beed9f7a
Chaykov <toumarenshi@gmail.com>
2024-06-24 22:35:36 +0200

    Commit 4: Installed bodyParser and added two new routes: admin and shop.

--- app.js
+++ app.js
@@ -1,15 +1,16 @@
 const express = require("express");
+const bodyParser = require("body-parser");
+
+const adminRoutes = require('./routes/admin');
+const shopRoutes = require('./routes/shop');
 
 const app = express();
+app.use(bodyParser.urlencoded({ extended: true }));
 
-app.use((req, res, next) => {
-  console.log("In the middleware");
-  next();
-});
+// app.use(express.json());
+// app.use(express.urlencoded());
 
-app.use((req, res, next) => {
-  console.log('In another middleware');
-  res.send('<h1>Hello from Express</h1>')
-})
+app.use(adminRoutes)
+app.use(shopRoutes)
 
 app.listen(3000, () => console.log("Server is running on port 3000"));
