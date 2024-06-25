93da34fc5ede829b80e035d1494605e483aca21f
Chaykov <toumarenshi@gmail.com>
2024-06-25 21:13:23 +0200

    Commit 5: Initialized some npm and added some styling for html.

--- app.js
+++ app.js
@@ -1,16 +1,22 @@
 const express = require("express");
+const path = require("path");
 const bodyParser = require("body-parser");
 
-const adminRoutes = require('./routes/admin');
+const adminData = require('./routes/admin');
 const shopRoutes = require('./routes/shop');
 
 const app = express();
 app.use(bodyParser.urlencoded({ extended: true }));
+app.use(express.static(path.join(__dirname, 'public')));
 
 // app.use(express.json());
 // app.use(express.urlencoded());
 
-app.use(adminRoutes)
+app.use('/admin/', adminData.routes)
 app.use(shopRoutes)
+
+app.use((req,res,next) => {
+    res.status(404).sendFile(path.join(__dirname, 'views', '404.html'));
+})
 
 app.listen(3000, () => console.log("Server is running on port 3000"));
