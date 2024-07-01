ca06765a5ae8278a04c856c9d0e718df747b9601
Chaykov <toumarenshi@gmail.com>
2024-07-01 22:42:32 +0200

    Commit 6: Installed some three engines for html like ejs, pug and express-handlebars for view engine.

--- app.js
+++ app.js
@@ -6,6 +6,10 @@ const adminData = require('./routes/admin');
 const shopRoutes = require('./routes/shop');
 
 const app = express();
+
+app.set('view engine', 'pug');
+app.set('views', 'views');
+
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static(path.join(__dirname, 'public')));
 
